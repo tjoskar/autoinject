91a571e1bd7634f6b93b722858cc0afe81b64906
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-07-22 23:34:34 +0200

    Add a index.js file

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+exports.__esModule = true;
+
+var _distSrcAutoInject = require('./dist/src/autoinject');
+
+exports.autoInject = _distSrcAutoInject.autoInject;
+exports.autoInstantiate = _distSrcAutoInject.autoInstantiate;
