af6b36003acb64c8865d0faab6a1e48e3729aa81
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-07-22 20:05:49 +0200

    Add examples

---
+++ example/example.ts
@@ -0,0 +1,21 @@
+import {autoInject, autoInstantiate} from '../src/autoinject';
+
+class User {
+    test = 'It works fine';
+}
+
+@autoInject // can be @autoInstantiate
+class Db {
+    constructor(public user: User) {}
+}
+
+@autoInstantiate
+class MyClass {
+    constructor(public db?: Db) {}
+}
+
+var k = new MyClass;
+
+console.log(Db.inject);
+
+console.log(k.db.user.test);
