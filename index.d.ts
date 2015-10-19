ffea48eff95a26bb96616c77ce1d64d649417a49
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-10-19 21:21:43 +0200

    issue-2: Make IoC public

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,5 @@ declare module "autoinject" {
     export function autoInject(target: any): any;
     export function autoInstantiate(target: any): any;
     export function inject(obj: any): any;
+    export function dependencyInjection(Cls: any): any
 }
--- index.js
+++ index.js
@@ -4,7 +4,9 @@ exports.__esModule = true;
 
 var _distSrcAutoInject = require('./dist/src/autoinject');
 var _distSrcInject = require('./dist/src/inject');
+var _distSrcIoc = require('./dist/src/ioc');
 
 exports.autoInject = _distSrcAutoInject.autoInject;
 exports.autoInstantiate = _distSrcAutoInject.autoInstantiate;
 exports.inject = _distSrcInject.inject;
+exports.dependencyInjection = _distSrcIoc.dependencyInjection;
