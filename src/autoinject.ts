53fa2b9d9c6b6692f3c8fd24ced17474a48f5c7b
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-12-03 21:09:31 +0100

    :bug: ISSUE-6: Remove all `Object` injects
    Removing `Object` from the inject array

--- src/autoinject.ts
+++ src/autoinject.ts
@@ -10,7 +10,16 @@ function autoInject(target): any {
         throw new Error('autoInject: Target must be a function');
     }
 
-    target.inject = Reflect.getOwnMetadata(parameterKey, target) || [];
+    const injects = Reflect.getOwnMetadata(parameterKey, target) || [];
+
+    // If a user use default parameters we will get `Object` as argument
+    // which will remove the default parameter. That is why we should replace
+    // the argument with undefined so the use are still able to use the default
+    // parameter. However, this will make it impossible to create a class with
+    // `Object` as argument :(
+    // See issue-6: https://github.com/tjoskar/autoinject/issues/6
+    target.inject = injects.map(i => i === Object ? undefined : i);
+
     return target;
 }
 
